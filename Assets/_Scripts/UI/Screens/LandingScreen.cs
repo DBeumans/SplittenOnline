9a2482458907e837adabba51e9344e7b28b01736
DBeumans <buncho.gamingnetwork@gmail.com>
2019-11-13 19:29:22 +0100

    Added UIController, UIScreen, UIButton scripts to manage the UI. Added Textmesh Pro. Renamed Samplescene to Main.

---
+++ Assets/_Scripts/UI/Screens/LandingScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Splitten.UI;
+
+public class LandingScreen : UIScreen
+{
+    [SerializeField] private UIButton proceedButton;
+
+    private void OnEnable()
+    {
+        proceedButton.OnButtonClick += () =>
+        {
+            base.UIController.GoToScreen<MainScreen>();
+        };
+    }
+
+    public override void EnableScreen()
+    {
+        base.EnableScreen();
+    }
+    public override void DisableScreen()
+    {
+        base.DisableScreen();
+    }
+}
---
+++ Assets/_Scripts/UI/UIController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Splitten.Extensions;
+
+namespace Splitten.UI
+{
+    public class UIController : Singleton<UIController>
+    {
+        [SerializeField]private UIScreen activeScreen;
+
+        [SerializeField]private List<UIScreen> screens = new List<UIScreen>();
+        private void Awake()
+	    {
+            this.FetchScreens();
+
+            this.activeScreen = this.screens[0];
+            this.activeScreen.EnableScreen();
+	    }
+
+        /// <summary>
+        /// Change from UI Screen
+        /// </summary>
+        /// <param name="OnScreenChanged">Callback when screen has been changed, returning the newly changed screen</param>
+        /// <typeparam name="T">UIScreen class to change to</typeparam>
+        public void GoToScreen<T>(Action<UIScreen> OnScreenChanged = null)
+        {
+            this.GoToScreen(typeof(T), OnScreenChanged);
+        }
+
+        /// <summary>
+        /// Change from UI Screen
+        /// </summary>
+        /// <param name="type">UIScreen class to change to</param>
+        /// <param name="OnScreenChanged">Callback when screen has been changed, returning the newly changed screen</param>
+        public void GoToScreen(System.Type type, Action<UIScreen> OnScreenChanged = null)
+        {
+            this.activeScreen.DisableScreen();
+
+            int screensLength = this.screens.Count;
+            for(int i = 0; i < screensLength; i++)
+            {
+                UIScreen currentScreen = this.screens[i];
+                if(!type.Equals(currentScreen.GetType()))
+                    continue;
+                
+                this.activeScreen = currentScreen;
+            }
+
+            this.activeScreen.EnableScreen();
+            OnScreenChanged?.Invoke(this.activeScreen);
+        }
+        
+    	/// <summary>
+        /// Fetching all the UIScreens inside the UIController GameObject.
+        /// </summary>
+        private void FetchScreens()
+        {
+            UIScreen[] uiScreens = this.GetComponents<UIScreen>();
+
+            int screensLength = uiScreens.Length;
+            for (int i = 0; i < screensLength; i++)
+            {
+                this.screens.Add(uiScreens[i]);
+            }
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
